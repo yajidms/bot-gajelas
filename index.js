022ffd8b30f02518f305e5fdd30814eb8d5bbc83
Yazid Alrasyid <yazidalrasyid145@gmail.com>
2025-04-17 22:56:35 +0700

    refactor: update downloader handlers and add Twitter support

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-require('dotenv').config(); 
+require('dotenv').config();
 const { Client, GatewayIntentBits, Collection } = require('discord.js');
 const { loadCommands } = require('./handlers/commandLoader');
 const { loadEvents } = require('./handlers/eventLoader');
-const { handleIgDownload, handleFbDownload, handleTwitterDownload } = require('./handlers/downloaderHandler');
+const { registerCommands } = require('./registerCommands');
+const { handleIg, handleFb, handleTt } = require('./handlers/downloaderHandler'); // Perubahan di sini
+const { handleYtDownload } = require('./handlers/ytdlHandler');
+const { handleX } = require('./handlers/twitterHandler');
 
 const client = new Client({ 
     intents: [
@@ -12,6 +15,7 @@ const client = new Client({
         GatewayIntentBits.GuildMembers
     ]
 });
+
 client.commands = new Collection();
 
 // Load commands and events
@@ -20,11 +24,13 @@ loadCommands(client);
 
 client.on('messageCreate', async (message) => {
     if (message.author.bot) return;
-    handleIgDownload(message);
-    handleFbDownload(message);
-    handleTwitterDownload(message);
+    if (message.content.startsWith('f.x')) handleX(message);
+    if (message.content.startsWith('f.ig')) handleIg(message);
+    if (message.content.startsWith('f.fb')) handleFb(message);
+    if (message.content.startsWith('f.tt')) handleTt(message);
+    if (message.content.startsWith('f.yt')) handleYtDownload(message);
 });
 
-
 client.login(process.env.DISCORD_TOKEN).then(() => {
+    registerCommands(client);
 });
