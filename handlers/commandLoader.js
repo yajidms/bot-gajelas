17db0187e2f29490b852037c0a5cb329f58d45ad
Yazid Alrasyid <yazidalrasyid145@gmail.com>
2025-04-28 20:32:28 +0700

    refactor: enhance command loading by recursively fetching command files and updating dependencies

--- handlers/commandLoader.js
+++ handlers/commandLoader.js
@@ -1,16 +1,32 @@
-const fs = require('fs');
+// filepath: d:\bot-gajelas\handlers\commandLoader.js
+const fs = require("fs");
+const path = require("path");
+
+function getAllCommandFiles(dir, files = []) {
+  fs.readdirSync(dir).forEach((file) => {
+    const fullPath = path.join(dir, file);
+    if (fs.statSync(fullPath).isDirectory()) {
+      getAllCommandFiles(fullPath, files);
+    } else if (file.endsWith(".js")) {
+      files.push(fullPath);
+    }
+  });
+  return files;
+}
 
 module.exports = {
-    loadCommands: (client) => {
-        const commands = [];
-        const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+  loadCommands: (client) => {
+    const commands = [];
+    const commandFiles = getAllCommandFiles(
+      path.join(__dirname, "../commands")
+    );
 
-        for (const file of commandFiles) {
-            const command = require(`../commands/${file}`);
-            client.commands.set(command.data.name, command);
-            commands.push(command.data.toJSON());
-        }
+    for (const file of commandFiles) {
+      const command = require(file);
+      client.commands.set(command.data.name, command);
+      commands.push(command.data.toJSON());
+    }
 
-        console.log('Commands berhasil dimuat dan didaftarkan.');
-    },
+    console.log("Commands berhasil dimuat dan didaftarkan.");
+  },
 };
